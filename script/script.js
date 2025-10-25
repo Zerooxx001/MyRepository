bfc058419a7e593deafacd11cac6dc2b02bc2665
Zerooxx001 <Doublezeroone505@gmail.com>
2025-10-25 19:25:41 +0000

    script changes

--- script/script.js
+++ script/script.js
@@ -1,16 +1,16 @@
-const boton = document.getElementsByClassName('mostrar-mensaje');
-const cuadro = document.getElementsByClassName('cuadro-flotante');
+const boton = document.querySelector('mostrar-mensaje');
+const cuadro = document.querySelector('cuadro-flotante');
 
 boton.addEventListener('click', () => {
     if (cuadro.style.display === 'none' || cuadro.style.display === '') {
-        cuadro.style.display= 'block';
+        cuadro.style.display = 'block';
     } else {
         cuadro.style.display = 'none';
     }
 });
 
 document.addEventListener('click', (e) => {
-    if (!cuadro.contains(e.target) && e.target !== boton) {
+    if (!cuadro.contains(e.target) && !boton.contains(e.target)) {
         cuadro.style.display = 'none';
     }
 });
