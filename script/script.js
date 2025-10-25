717b481e9d34d20782d3442a22ae0e81caf4dfcb
Zerooxx001 <Doublezeroone505@gmail.com>
2025-10-25 19:31:50 +0000

    script cambios

--- script/script.js
+++ script/script.js
@@ -2,16 +2,16 @@ const boton = document.querySelector('.mostrar-mensaje');
 const cuadro = document.querySelector('.cuadro-flotante');
 
 boton.addEventListener('click', () => {
-    if (cuadro.style.display === 'none' || cuadro.style.display === '') {
-        cuadro.style.display = 'block';
-    } else {
-        cuadro.style.display = 'none';
-    }
+  // Alternar visibilidad
+  if (cuadro.style.display === 'none' || cuadro.style.display === '') {
+    cuadro.style.display = 'block';
+  } else {
+    cuadro.style.display = 'none';
+  }
 });
 
 document.addEventListener('click', (e) => {
-    if (!cuadro.contains(e.target) && !boton.contains(e.target)) {
-        cuadro.style.display = 'none';
-            
-        }
+  if (!cuadro.contains(e.target) && !boton.contains(e.target)) {
+    cuadro.style.display = 'none';
+  }
 });
